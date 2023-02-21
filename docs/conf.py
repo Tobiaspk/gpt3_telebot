dec72ed5f5563d06fd3db3964ede2d7a39eb14ee
tobiaspk1 <tobiaspk1@gmail.com>
2023-02-21 01:47:22 -0500

    Update appearance

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '2023-02-15'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
